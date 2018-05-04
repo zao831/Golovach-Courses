bc3c03fb7655dfa9efa6100ef1a64fa998f505af
zao831 <zao831@gmail.com>
2018-05-04 21:17:26 +0300

    Create package lecture_08 and added file PrintThread.java

---
+++ lecture_8/PrintThread.java
@@ -0,0 +1,28 @@
+package lecture_8;
+
+/*
+ * Golovach Courses
+ * Java Core: Потоки. Лекция #8
+ * https://youtu.be/0FvbIGNUl8A
+ */
+
+public class PrintThread implements Runnable {
+	String b;
+	int a;
+	public PrintThread(String name, int time) {
+		 this.b = name;
+		 this.a = time;
+	}
+	
+	@Override
+	public void run() {
+		 try {
+			for (int i = 0; i < 10; i++) {
+				Thread.sleep(a);
+				System.out.print(b + "\n");
+			}
+		} catch (InterruptedException e) {
+			 System.out.println(e);
+		}
+	}
+}
